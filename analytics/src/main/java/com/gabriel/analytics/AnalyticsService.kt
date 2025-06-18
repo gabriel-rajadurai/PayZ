e852b3abf8eb3298200d4de1eefd64243faddd51
Gabriel <rajadurai.gabriel@gmail.com>
2025-06-18 11:24:16 +0530

    Setup project

---
+++ analytics/src/main/java/com/gabriel/analytics/AnalyticsService.kt
@@ -0,0 +1,12 @@
+package com.gabriel.analytics
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class AnalyticsService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
